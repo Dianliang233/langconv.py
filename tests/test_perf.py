4f6e4881082c4bcabfd5561592be4bf72a92b6e0
Dianliang233 <dianliang233@gmail.com>
2023-06-16 15:12:48 +0800

    Fix prof filename

--- tests/test_perf.py
+++ tests/test_perf.py
@@ -8,4 +8,4 @@ def test_perf(benchmark):
     lc = LanguageConverter.from_language(zh_cn)
     with open('tests/zh_cn.txt', encoding='utf-8') as large_txt:
         content = large_txt.read() * 5
-        benchmark(cProfile.runctx, 'lc.convert(content)', None, locals(), 'stats.perf')
+        benchmark(cProfile.runctx, 'lc.convert(content)', None, locals(), 'stats.prof')
